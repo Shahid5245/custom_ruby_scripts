b501dd20634c0fa7044fe37c1436d984e0c03509
Shahid5245 <mohamedshahid5245@gmail.com>
2025-01-17 15:47:40 +0530

    bug fix

--- loader.rb
+++ loader.rb
@@ -2,6 +2,12 @@
 
 git_base_url = 'https://raw.githubusercontent.com/Shahid5245/custom_ruby_scripts/main/'
 files_to_load = %w[result_hash/result_hash_v6_script.rb result_hash/validation_result_hash.rb my_custom_methods.rb]
+AllTenants = Utils::Property.new.get_all_active_tenant_key - ['qa-tenant']
+OrgAffProfile = PROFILE_URL['organization_affiliation']
+PrRoleProfile = PROFILE_URL['practitioner_role']
+NppesProfile = PROFILE_URL['nppes_master']
+ChewyClientIndex = Chewy.client.indices
+ElasticClientIndex = ELASTICSEARCH_CLIENT.indices
 
 # Display options
 DisplayOptions = ->(title, options) {
@@ -15,9 +21,3 @@ files_to_load = load_choice == 2 ? [files_to_load.last] : files_to_load
 
 files_to_load.each { |filename| eval(URI.open(git_base_url + filename).read) }
 
-AllTenants = Utils::Property.new.get_all_active_tenant_key - ['qa-tenant']
-OrgAffProfile = PROFILE_URL['organization_affiliation']
-PrRoleProfile = PROFILE_URL['practitioner_role']
-NppesProfile = PROFILE_URL['nppes_master']
-ChewyClientIndex = Chewy.client.indices
-ElasticClientIndex = ELASTICSEARCH_CLIENT.indices
